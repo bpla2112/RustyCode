071870496342748a7ac6633650793dbf423f916a
Bernardo Pla <bpla2112@gmail.com>
2017-05-17 23:45:00 -0400

    Initial commit
    Files from Rust online book are here.

---
+++ Tutorials/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\bpla\Documents\GitHub\ -\ Personal\RustyCode\Tutorials\hello_world\target\debug\hello_world.exe: C:\Users\bpla\Documents\GitHub\ -\ Personal\RustyCode\Tutorials\hello_world\src\main.rs
